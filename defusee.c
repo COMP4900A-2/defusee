be3292e781513f5d4a3fcc292c2ea32a67981aab
William Wright <williamwright3@cmail.carleton.ca>
2022-11-16 19:12:51 -0500

    nov 16 work

--- defusee.c
+++ defusee.c
@@ -3,6 +3,8 @@
 #include <sys/neutrino.h>
 #include <pthread.h>
 #include <sched.h>
+#include <fcntl.h>
+#include <termios.h>
 
 #include "defs.h"
 
@@ -11,6 +13,12 @@ void *movement(void *);
 
 int main(void) {
 
+	struct termios attr;
+	fcntl(0, F_SETFL, O_NONBLOCK);
+	tcgetattr(0, &attr);
+	attr.c_lflag &= ~ICANON;
+	attr.c_lflag &= ECHO;
+	tcsetattr(0, TCSANOW, &attr);
 
 	Info info;
 	info.x = 50;
@@ -28,6 +36,9 @@ void *sensor(void * arg) {
 	Info *info = (Info *) arg;
 	int coid = name_open(ATTACH_POINT,0);
 	while(1) {
+		int x = getchar();
+		printf("%d\n", x);
+		sleep(1);
 		MsgSend(coid, info, sizeof(*info), info, sizeof(*info));
 	}
 }
@@ -36,7 +47,9 @@ void *movement(void *arg) {
 	Info *info = (Info *) arg;
 	//wait for a command to move
 	while(1) {
-		// conditions here and also mutex and condvar stuff
+		// 1. getchar() to get '/033'
+		// if it is: we discard the next character, and we only care about the A,B,C,D,E.
+		//if it's not, we ignore it
 		info->y += 1;
 		sleep(1);
 	}
